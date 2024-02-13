321327bf92ac80259a62eefe0feb6fa7e17d82e8
lrebuffi <lrebuffi@anl.gov>
2024-02-13 11:47:22 -0600

    new hybrid structure

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ from setuptools import find_packages, setup
 import subprocess
 
 NAME = 'OASYS1-shadow4'
-VERSION = '0.0.21'
+VERSION = '0.0.22'
 ISRELEASED = False
 
 DESCRIPTION = 'oasys-shadow4: Oasys widgets for shadow4'
@@ -91,9 +91,9 @@ SETUP_REQUIRES = (
 
 INSTALL_REQUIRES = (
                     'oasys1>=1.2.140',
-                    'shadow4>=0.1.21',
+                    'shadow4>=0.1.22',
                     'xoppylib', # used in Bragg preprocessor... todo: maybe move that part to crystalpy?
-                    'oasys-hybrid-methods>=1.0.6'
+                    'shadow4-hybrid>=0.0.2'
                     )
 
 PACKAGES = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests'))
