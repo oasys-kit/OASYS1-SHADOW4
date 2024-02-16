04841c1bb77fb9bd2f07830e71bcca38b6291eb4
lrebuffi <lrebuffi@anl.gov>
2024-02-15 18:55:43 -0600

    s4hybrid to python script Hybrid for KB

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ from setuptools import find_packages, setup
 import subprocess
 
 NAME = 'OASYS1-shadow4'
-VERSION = '0.0.26'
+VERSION = '0.0.27'
 ISRELEASED = False
 
 DESCRIPTION = 'oasys-shadow4: Oasys widgets for shadow4'
@@ -91,9 +91,9 @@ SETUP_REQUIRES = (
 
 INSTALL_REQUIRES = (
                     'oasys1>=1.2.140',
-                    'shadow4>=0.1.22',
+                    'shadow4>=0.1.23',
                     'xoppylib', # used in Bragg preprocessor... todo: maybe move that part to crystalpy?
-                    'shadow4-hybrid>=0.0.5'
+                    'shadow4-hybrid>=0.0.6'
                     )
 
 PACKAGES = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests'))
