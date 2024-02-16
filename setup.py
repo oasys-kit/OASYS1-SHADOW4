2513d0db73ee410c14c210b0c5179e422f5c13fc
lrebuffi <lrebuffi@anl.gov>
2024-02-15 18:49:24 -0600

    s4hybrid to python script Hybrid for KB

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ from setuptools import find_packages, setup
 import subprocess
 
 NAME = 'OASYS1-shadow4'
-VERSION = '0.0.25'
+VERSION = '0.0.26'
 ISRELEASED = False
 
 DESCRIPTION = 'oasys-shadow4: Oasys widgets for shadow4'
