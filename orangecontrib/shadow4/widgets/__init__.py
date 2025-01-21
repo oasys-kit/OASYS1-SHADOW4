d495c9341955a33dc88aea0df553292e62e10c83
lrebuffi <lrebuffi@anl.gov>
2025-01-21 12:27:56 -0600

    replaced deprecated pkg_resources with importlib

--- orangecontrib/shadow4/widgets/__init__.py
+++ orangecontrib/shadow4/widgets/__init__.py
@@ -1,2 +1,3 @@
 # namespace declaration.
-__path__ = __import__("importlib.util").find_spec(__name__).submodule_search_locations
+__import__("pkg_resources").declare_namespace(__name__)
+#__path__ = __import__("importlib.util").find_spec(__name__).submodule_search_locations
