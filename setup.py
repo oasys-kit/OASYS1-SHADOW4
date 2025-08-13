a26623c9b3aa45cf26ff13938b06978afb63e559
Manuel Sanchez del Rio <srio@esrf.eu>
2025-08-13 11:20:03 +0200

    update version

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ import sys
 from setuptools import find_packages, setup
 
 NAME = 'OASYS1-shadow4'
-VERSION = '0.0.73'
+VERSION = '0.0.74'
 ISRELEASED = False
 
 DESCRIPTION = 'oasys-shadow4: Oasys widgets for shadow4'
